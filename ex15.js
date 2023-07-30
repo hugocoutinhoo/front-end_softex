1082eb49c0d92586bae14ddaed70136662e350ca
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-07-30 10:43:37 -0300

    Update ex15.js

--- ex15.js
+++ ex15.js
@@ -1,5 +1,8 @@
 /*
-Faça um código, utilizando o Google Blockly, que execute a contagem regressiva de uma bomba, informando o número de segundos para explodir. Ele deverá mostrar a mensagem “iniciando contagem regressiva”, os segundos passados e, no final, a mensagem “BUM!”.
+Faça um código, utilizando o Google Blockly, que execute a contagem regressiva de uma bomba, informando o número de segundos 
+para explodir. Ele deverá mostrar a mensagem 
+“iniciando contagem regressiva”, os segundos passados 
+e, no final, a mensagem “BUM!”.
 
 */
 
